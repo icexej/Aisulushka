844353c051d300213af1d20b10f6204cc896721c
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-28 14:30:56 +0600

    task1-20

---
+++ src/task5.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class task5 {
+    public static void main(String[] args) {
+
+        ArrayList<String> colors  = new ArrayList<>(Arrays.asList("Red","Green","Blue","White","Pink"));
+        Collections.fill(colors,"Black");
+
+        System.out.println(colors);
+    }
+}
