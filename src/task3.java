d86cf5ff5e76cf90ff77c1253ad1c627b76039b9
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-28 12:23:19 +0600

    task1-20

---
+++ src/task3.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class task3 {
+    public static void main(String[] args) {
+
+        ArrayList<String> foods  = new ArrayList<>();
+        foods.add("Pizza");
+        foods.add("Sushi");
+        foods.add("Cola");
+        foods.add("Pizza");
+        foods.add("Pasta");
+        foods.add("Manty");
+        foods.add("Fri");
+
+        int pizzaCount = 0;
+                for(String food: foods){
+                    if (food.equals("Pizza")){
+                        pizzaCount++;
+                    }
+                }
+        System.out.println("Count of Pizza in the arraylist " + pizzaCount);
+    }
+}
