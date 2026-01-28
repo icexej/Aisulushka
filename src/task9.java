a99c66c14574bddb3e5403ea75ddd27f405ee60c
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-28 15:03:40 +0600

    task1-20

---
+++ src/task9.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class task9 {
+    public static void main(String[] args) {
+
+        ArrayList<String> cities  = new ArrayList<>(Arrays.asList("Bishkek","Moscow","Paris","Pecin","Tokyo"));
+        String[] cityArray = cities.toArray(new String[0]);
+
+
+        System.out.println(cityArray[0] );
+
+    }
+}
