38a344442112f6767218aab3886526525feb364c
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-28 12:08:21 +0600

    hello) the task 2

---
+++ src/Max.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Max {
+    public static void main(String[] args) {
+
+        ArrayList<String> movies  = new ArrayList<>();
+        movies.add("1984");
+        movies.add("Dune");
+        movies.add("Harry Potter");
+        movies.add("Brave New World");
+
+        String longestMovie = "";
+        for (String movie: movies) {
+            if (movie.length()>longestMovie.length()) {
+                longestMovie = movie;
+            }
+        }
+        System.out.println("The longest tittle is " + longestMovie);
+    }
+}
