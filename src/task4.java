2385fd809331a9a22b302f38ce3d4b60c94d204e
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-28 14:20:07 +0600

    task1-20

---
+++ src/task4.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class task4 {
+    public static void main(String[] args) {
+
+        ArrayList<String> fruits  = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Pomelo");
+        fruits.add("Banana");
+        fruits.add("Pineapple");
+        fruits.add("Kiwi");
+        fruits.add("Mango");
+
+        fruits.removeIf(f -> f.equals("Banana"));
+        System.out.println("After deleting: " + fruits);
+    }
+}
