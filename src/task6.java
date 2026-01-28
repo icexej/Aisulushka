208bbe6d8727fe308dbe371eb3b07689e6231305
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-28 14:43:20 +0600

    task1-20

---
+++ src/task6.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class task6 {
+    public static void main(String[] args) {
+
+        ArrayList<String> sports1  = new ArrayList<>(Arrays.asList("Football","Basketball","Tennis","Swimming","Ping-Pong"));
+        ArrayList<String> sports2  = new ArrayList<>(Arrays.asList("Football","Tennis","Ping-Pong"));
+        boolean result = sports1.containsAll(sports2);
+        if(result) {
+            System.out.println("Yes, in the general list has all of these sports");
+        } else {
+            System.out.println("No, there are not some types of sport");
+        }
+    }
+}
