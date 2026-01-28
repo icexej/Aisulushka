a6e47d5dfee8ffae7ff1dce6836397256200e3b5
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-28 14:54:14 +0600

    task1-20

---
+++ src/task7.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class task7 {
+    public static void main(String[] args) {
+
+        ArrayList<String> flowers  = new ArrayList<>(Arrays.asList("Rose","Lily","Rose","Peony","Sunflower"));
+
+        int first = flowers.indexOf("Rose");
+        int last = flowers.lastIndexOf("Rose");
+        System.out.println("First index of Rose: " + first );
+        System.out.println("Last index of Rose: " + last );
+    }
+}
