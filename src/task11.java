9681725f4a9aee87593a6a5643e878c153125309
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-30 14:32:26 +0600

    task11

--- src/task11.java
+++ src/task11.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 public class task11 {
     public static void main(String[] args) {
 
-        ArrayList<String> countries1  = new ArrayList<>(Arrays.asList("Russia","German","France","Japan","China"));
-        ArrayList<String> countries2  = new ArrayList<>(Arrays.asList("Canada","France","Japan"));
+        ArrayList<String> countries1 = new ArrayList<>(Arrays.asList("Russia", "German", "France", "Japan", "China"));
+        ArrayList<String> countries2 = new ArrayList<>(Arrays.asList("Canada", "France", "Japan"));
 
         ArrayList<String> commonElements = new ArrayList<>(countries1);
         commonElements.retainAll(countries2);
