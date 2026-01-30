75cc8c8d16f48ac1569449a4ec57f415ea42130a
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-30 14:34:45 +0600

    task02

--- src/task02.java
+++ src/task02.java
@@ -3,15 +3,15 @@ import java.util.ArrayList;
 public class task02 {
     public static void main(String[] args) {
 
-        ArrayList<String> movies  = new ArrayList<>();
+        ArrayList<String> movies = new ArrayList<>();
         movies.add("1984");
         movies.add("Dune");
         movies.add("Harry Potter");
         movies.add("Brave New World");
 
         String longestMovie = "";
-        for (String movie: movies) {
-            if (movie.length()>longestMovie.length()) {
+        for (String movie : movies) {
+            if (movie.length() > longestMovie.length()) {
                 longestMovie = movie;
             }
         }
