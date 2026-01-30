ff66945c13670dbc42b33c7f783410dd39a14494
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-30 15:27:55 +0600

    task13

--- src/task13.java
+++ src/task13.java
@@ -5,9 +5,9 @@ public class task13 {
     public static void main(String[] args) {
         ArrayList<String> songs = new ArrayList<>(Arrays.asList("Come and go", "Iris", "Heartless", "Often", "Streets"));
         String shortestSong = songs.get(0);
-        for (int i = 1; i<songs.size(); i++){
+        for (int i = 1; i < songs.size(); i++) {
             String currentSong = songs.get(i);
-            if (currentSong.length()<shortestSong.length()){
+            if (currentSong.length() < shortestSong.length()) {
                 shortestSong = currentSong;
             }
         }
