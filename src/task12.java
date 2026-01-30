7881e5fa0164adb7632107c1ec063f432df86a3a
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-30 15:27:32 +0600

    task12

--- src/task12.java
+++ src/task12.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 public class task12 {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<>(Arrays.asList("Aisulu", "Diana", "Erjan", "Danila", "Muslima"));
+        System.out.println("Original: " + names);
         names.removeIf(name -> name.length() % 2 == 0);
         System.out.println("After deleting: " + names);
     }
