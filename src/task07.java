c8bbc8762683d3929348bb49bf78625542e4a566
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-30 14:38:17 +0600

    task07

--- src/task07.java
+++ src/task07.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 public class task07 {
     public static void main(String[] args) {
 
-        ArrayList<String> flowers  = new ArrayList<>(Arrays.asList("Rose","Lily","Rose","Peony","Sunflower"));
+        ArrayList<String> flowers = new ArrayList<>(Arrays.asList("Rose", "Lily", "Rose", "Peony", "Sunflower"));
 
         int first = flowers.indexOf("Rose");
         int last = flowers.lastIndexOf("Rose");
-        System.out.println("First index of Rose: " + first );
-        System.out.println("Last index of Rose: " + last );
+        System.out.println("First index of Rose: " + first);
+        System.out.println("Last index of Rose: " + last);
     }
 }
