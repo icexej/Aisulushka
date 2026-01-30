fb0a42346f05bf366526032dabfac0b4fb1570de
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-30 14:39:33 +0600

    task10

--- src/task10.java
+++ src/task10.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 public class task10 {
     public static void main(String[] args) {
 
-        Integer[] numbersArray ={1,2,3,4,5,6,7,8,9,10};
+        Integer[] numbersArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         ArrayList<Integer> numbersList = new ArrayList<>(Arrays.asList(numbersArray));
 
-        System.out.println(numbersList );
+        System.out.println(numbersList);
 
     }
 }
