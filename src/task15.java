184746f591c0bc8e8b27832c7bf1085e4e10a18b
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-30 15:16:42 +0600

    task15

---
+++ src/task15.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class task15 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(12,54,37,82,27,834,90,56,23));
+        ArrayList<Integer> evenList = new ArrayList<>();
+        ArrayList<Integer> oddList = new ArrayList<>();
+        for (Integer num: numbers){
+            if (num%2==0) {
+                evenList.add(num);
+            } else {
+                oddList.add(num);
+            }
+        }
+
+        System.out.println("Even numbers: " + evenList);
+        System.out.println("Odd numbers: " + oddList);
+
+    }
+}
