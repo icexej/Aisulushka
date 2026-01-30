9913b31378343e7f8e9ec81152e3b967c7dc0d6e
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-30 14:38:46 +0600

    task08

--- src/task08.java
+++ src/task08.java
@@ -6,11 +6,11 @@ import java.util.Set;
 public class task08 {
     public static void main(String[] args) {
 
-        ArrayList<String> animals  = new ArrayList<>(Arrays.asList("Dog","Cow","Cat","Bird","Cat"));
-        Set<String> set=new LinkedHashSet<>(animals);
+        ArrayList<String> animals = new ArrayList<>(Arrays.asList("Dog", "Cow", "Cat", "Bird", "Cat"));
+        Set<String> set = new LinkedHashSet<>(animals);
         animals = new ArrayList<>(set);
 
-        System.out.println(animals );
+        System.out.println(animals);
 
     }
 }
