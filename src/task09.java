907f48a3c9ad7e7dccced878a23fcfb6d945117c
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-30 14:39:12 +0600

    task09

--- src/task09.java
+++ src/task09.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 public class task09 {
     public static void main(String[] args) {
 
-        ArrayList<String> cities  = new ArrayList<>(Arrays.asList("Bishkek","Moscow","Paris","Pecin","Tokyo"));
+        ArrayList<String> cities = new ArrayList<>(Arrays.asList("Bishkek", "Moscow", "Paris", "Pecin", "Tokyo"));
         String[] cityArray = cities.toArray(new String[0]);
 
 
-        System.out.println(cityArray[0] );
+        System.out.println(cityArray[0]);
 
     }
 }
