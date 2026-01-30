baef4cc55149aa965b2cf3167c67fd6bafc564a9
icexej <alymbekovaaisulu6a@gmail.com>
2026-01-30 14:59:20 +0600

    task13

---
+++ src/task13.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class task13 {
+    public static void main(String[] args) {
+        ArrayList<String> songs = new ArrayList<>(Arrays.asList("Come and go", "Iris", "Heartless", "Often", "Streets"));
+        String shortestSong = songs.get(0);
+        for (int i = 1; i<songs.size(); i++){
+            String currentSong = songs.get(i);
+            if (currentSong.length()<shortestSong.length()){
+                shortestSong = currentSong;
+            }
+        }
+        System.out.println("Rhe shortest name of song '" + shortestSong + "'");
+    }
+}
